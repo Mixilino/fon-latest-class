e935dae244f3136b508d8943987f018deefa2a4b
xo21 <38262508+xo21@users.noreply.github.com>
2019-04-07 12:33:38 +0200

    mixi

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fon-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'fon-latest';
+}
